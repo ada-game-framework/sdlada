7b99da80fa11c0b62bba3b077b53c96d4dc83f54
Luke A. Guest <laguest@archeia.com>
2013-09-24 13:19:11 +0100

    Corrected name conflicts. Made the names aliased. Moved C functions into their respective Ada subprograms.

--- src/sdl-hints.adb
+++ src/sdl-hints.adb
@@ -2,79 +2,107 @@ with Interfaces.C;
 with Interfaces.C.Strings;
 
 package body SDL.Hints is
-   Frame_Buffer_Acceleration        : constant String := "SDL_FRAMEBUFFER_ACCELERATION";
-   Render_Driver                    : constant String := "SDL_RENDER_DRIVER";
-   Render_OpenGL_Shaders            : constant String := "SDL_RENDER_OPENGL_SHADERS";
-   Render_Scale_Quality             : constant String := "SDL_RENDER_SCALE_QUALITY";
-   Render_VSync                     : constant String := "SDL_RENDER_VSYNC";
-   Video_X11_XVidMode               : constant String := "SDL_VIDEO_X11_XVIDMODE";
-   Video_X11_Xinerama               : constant String := "SDL_VIDEO_X11_XINERAMA";
-   Video_X11_XRandR                 : constant String := "SDL_VIDEO_X11_XRANDR";
-   Grab_Keyboard                    : constant String := "SDL_GRAB_KEYBOARD";
-   Video_Minimise_On_Focus_Loss     : constant String := "SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS";
-   Idle_Timer_Disabled              : constant String := "SDL_IOS_IDLE_TIMER_DISABLED";
-   IOS_Orientations                 : constant String := "SDL_IOS_ORIENTATIONS";
-   XInput_Enabled                   : constant String := "SDL_XINPUT_ENABLED";
-   Game_Controller_Config           : constant String := "SDL_GAMECONTROLLERCONFIG";
-   Joystick_Allow_Background_Events : constant String := "SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS";
-   Allow_Topmost                    : constant String := "SDL_ALLOW_TOPMOST";
-   Timer_Resolution                 : constant String := "SDL_TIMER_RESOLUTION";
+   package C renames Interfaces.C;
 
-   type Hint_Names is access String;
+   use type C.int;
+
+   Frame_Buffer_Acceleration_Name        : aliased constant String := "SDL_FRAMEBUFFER_ACCELERATION";
+   Render_Driver_Name                    : aliased constant String := "SDL_RENDER_DRIVER";
+   Render_OpenGL_Shaders_Name            : aliased constant String := "SDL_RENDER_OPENGL_SHADERS";
+   Render_Scale_Quality_Name             : aliased constant String := "SDL_RENDER_SCALE_QUALITY";
+   Render_VSync_Name                     : aliased constant String := "SDL_RENDER_VSYNC";
+   Video_X11_XVidMode_Name               : aliased constant String := "SDL_VIDEO_X11_XVIDMODE";
+   Video_X11_Xinerama_Name               : aliased constant String := "SDL_VIDEO_X11_XINERAMA";
+   Video_X11_XRandR_Name                 : aliased constant String := "SDL_VIDEO_X11_XRANDR";
+   Grab_Keyboard_Name                    : aliased constant String := "SDL_GRAB_KEYBOARD";
+   Video_Minimise_On_Focus_Loss_Name     : aliased constant String := "SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS";
+   Idle_Timer_Disabled_Name              : aliased constant String := "SDL_IOS_IDLE_TIMER_DISABLED";
+   IOS_Orientations_Name                 : aliased constant String := "SDL_IOS_ORIENTATIONS";
+   XInput_Enabled_Name                   : aliased constant String := "SDL_XINPUT_ENABLED";
+   Game_Controller_Config_Name           : aliased constant String := "SDL_GAMECONTROLLERCONFIG";
+   Joystick_Allow_Background_Events_Name : aliased constant String := "SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS";
+   Allow_Topmost_Name                    : aliased constant String := "SDL_ALLOW_TOPMOST";
+   Timer_Resolution_Name                 : aliased constant String := "SDL_TIMER_RESOLUTION";
+
+   type Hint_Names is access constant String;
 
    Hint_Name_Map : constant array (Hint'Range) of Hint_Names :=
-     (Frame_Buffer_Acceleration'Access,
-      Render_Driver'Access,
-      Render_OpenGL_Shaders'Access,
-      Render_Scale_Quality'Access,
-      Render_VSync'Access,
-      Video_X11_XVidMode'Access,
-      Video_X11_Xinerama'Access,
-      Video_X11_XRandR'Access,
-      Grab_Keyboard'Access,
-      Video_Minimise_On_Focus_Loss'Access,
-      Idle_Timer_Disabled'Access,
-      IOS_Orientations'Access,
-      XInput_Enabled'Access,
-      Game_Controller_Config'Access,
-      Joystick_Allow_Background_Events'Access,
-      Allow_Topmost'Access,
-      Timer_Resolution'Access);
+     (Frame_Buffer_Acceleration_Name'Access,
+      Render_Driver_Name'Access,
+      Render_OpenGL_Shaders_Name'Access,
+      Render_Scale_Quality_Name'Access,
+      Render_VSync_Name'Access,
+      Video_X11_XVidMode_Name'Access,
+      Video_X11_Xinerama_Name'Access,
+      Video_X11_XRandR_Name'Access,
+      Grab_Keyboard_Name'Access,
+      Video_Minimise_On_Focus_Loss_Name'Access,
+      Idle_Timer_Disabled_Name'Access,
+      IOS_Orientations_Name'Access,
+      XInput_Enabled_Name'Access,
+      Game_Controller_Config_Name'Access,
+      Joystick_Allow_Background_Events_Name'Access,
+      Allow_Topmost_Name'Access,
+      Timer_Resolution_Name'Access);
+
+   function Value (H : in Hint) return String with
+     Inline => True;
 
    function Value (H : in Hint) return String is
    begin
       return Hint_Name_Map (H).all;
    end Value;
 
-   function SDL_GetHint (C_Str : in C.char_array) return C.Strings.chars_ptr with
+   function SDL_Get_Hint (C_Str : in C.Strings.chars_ptr) return C.Strings.chars_ptr with
      Import        => True,
      Convention    => C,
      External_Name => "SDL_GetHint";
 
-   function SDL_SetHint (Name, Value : in C.char_array) return Boolean with
+   function SDL_Set_Hint (Name, Value : in C.Strings.chars_ptr) return C.int with
      Import        => True,
      Convention    => C,
      External_Name => "SDL_SetHint";
 
-   function SDL_SetHint (Name, Value : in C.Char_array; P : in Priorities) return Boolean with
+   function SDL_Set_Hint (Name, Value : in C.Strings.chars_ptr; P : in Priorities) return C.int with
      Import        => True,
      Convention    => C,
      External_Name => "SDL_SetHintWithPriority";
 
-   function Get (H : in Hint) return String is
-      C_Str : constant C.Strings.chars_Ptr := SDL_GetHint (C.To_C (Value (H)));
+   function Get (Name : in Hint) return String is
+      C_Hint_Str : C.Strings.chars_ptr := C.Strings.New_String (
+                                                                Value (H => Name));
+      C_Str      : constant C.Strings.chars_ptr := SDL_Get_Hint (C_Hint_Str);
    begin
+      C.Strings.Free (C_Hint_Str);
+
       return C.Strings.Value (C_Str);
    end Get;
 
-   function Set (H : in Hint; Value : in String) return Boolean is
+   function Set (Name : in Hint; Value : in String) return Boolean is
+      C_Hint_Str  : C.Strings.chars_ptr := C.Strings.New_String (Hints.Value (Name));
+      C_Value_Str : C.Strings.chars_ptr := C.Strings.New_String (Value);
+      Success     : C.int := SDL_Set_Hint
+        (Name  => C_Hint_Str,
+         Value => C_Value_Str);
    begin
-      return SDL_SetHint (C.To_C (Value (H)), C.To_C (Value));
+      C.Strings.Free (C_Hint_Str);
+      C.Strings.Free (C_Value_Str);
+
+      return (if Success = 1 then True else False);
    end Set;
 
-   function Set (H : in Hint; Value : in String; Priority : in Priorities)
+   function Set (Name : in Hint; Value : in String; Priority : in Priorities)
                 return Boolean is
+      C_Hint_Str  : C.Strings.chars_ptr := C.Strings.New_String (Hints.Value (Name));
+      C_Value_Str : C.Strings.chars_ptr := C.Strings.New_String (Value);
+      Success     : C.int := SDL_Set_Hint
+        (Name  => C_Hint_Str,
+         Value => C_Value_Str,
+         P     => Priority);
    begin
-      return SDL_SetHint (C.To_C (Value (H)), C.To_C (Value), Priority);
+      C.Strings.Free (C_Hint_Str);
+      C.Strings.Free (C_Value_Str);
+
+      return (if Success = 1 then True else False);
    end Set;
 end SDL.Hints;
