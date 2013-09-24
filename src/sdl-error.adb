7cbc452f213df783bbd4fe882b7401468cd273c3
Luke A. Guest <laguest@archeia.com>
2013-09-24 13:20:01 +0100

    Fixed C-ish names to Ada ones.

--- src/sdl-error.adb
+++ src/sdl-error.adb
@@ -4,23 +4,23 @@ with Interfaces.C.Strings;
 package body SDL.Error is
    package C renames Interfaces.C;
 
-   procedure SDL_SetError (C_Str : in C.char_array) with
+   procedure SDL_Set_Error (C_Str : in C.char_array) with
      Import        => True,
      Convention    => C,
      External_Name => "SDL_SetError";
 
-   function SDL_GetError return C.Strings.chars_ptr with
+   function SDL_Get_Error return C.Strings.chars_ptr with
      Import        => True,
      Convention    => C,
      External_Name => "SDL_GetError";
 
    procedure Set (S : in String) is
    begin
-      SDL_SetError (C.To_C (S));
+      SDL_Set_Error (C.To_C (S));
    end Set;
 
    function Get return String is
-      C_Str : C.Strings.chars_ptr := SDL_GetError;
+      C_Str : C.Strings.chars_ptr := SDL_Get_Error;
    begin
       return C.Strings.Value (C_Str);
    end Get;
