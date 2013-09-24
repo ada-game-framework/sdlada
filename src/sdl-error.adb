327c0c967cf898343b473cccb292a063fa951201
Luke A. Guest <laguest@archeia.com>
2013-09-24 13:25:40 +0100

    Moved the C functions inside the Ada subprograms.

--- src/sdl-error.adb
+++ src/sdl-error.adb
@@ -4,22 +4,21 @@ with Interfaces.C.Strings;
 package body SDL.Error is
    package C renames Interfaces.C;
 
-   procedure SDL_Set_Error (C_Str : in C.char_array) with
-     Import        => True,
-     Convention    => C,
-     External_Name => "SDL_SetError";
-
-   function SDL_Get_Error return C.Strings.chars_ptr with
-     Import        => True,
-     Convention    => C,
-     External_Name => "SDL_GetError";
-
    procedure Set (S : in String) is
+      procedure SDL_Set_Error (C_Str : in C.char_array) with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_SetError";
    begin
       SDL_Set_Error (C.To_C (S));
    end Set;
 
    function Get return String is
+      function SDL_Get_Error return C.Strings.chars_ptr with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_GetError";
+
       C_Str : C.Strings.chars_ptr := SDL_Get_Error;
    begin
       return C.Strings.Value (C_Str);
