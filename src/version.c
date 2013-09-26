1dd984422b7dbd8975e901284b832021f33292be
Luke A. Guest <laguest@archeia.com>
2013-09-26 14:35:55 +0100

    Added versioning.

---
+++ src/version.c
@@ -0,0 +1,7 @@
+#include <SDL.h>
+
+/* We need to define some constants here so we can get access to the values that in #define form from Ada.
+ */
+const Uint8 SDL_Ada_Major_Version = SDL_MAJOR_VERSION;
+const Uint8 SDL_Ada_Minor_Version = SDL_MINOR_VERSION;
+const Uint8 SDL_Ada_Patch_Version = SDL_PATCHLEVEL;
