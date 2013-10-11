88320b68e04550d826996e43ec7410ab0d574e99
Luke A. Guest <laguest@archeia.com>
2013-10-11 13:30:36 +0100

    Added more functions.

--- src/sdl-video-windows.adb
+++ src/sdl-video-windows.adb
@@ -1,8 +1,11 @@
+with Ada.Unchecked_Conversion;
 with Interfaces.C;
 with Interfaces.C.Strings;
 
 package body SDL.Video.Windows is
    package C renames Interfaces.C;
+
+   use type C.int;
 
    procedure Create
      (Self   : in out Window;
@@ -30,6 +33,15 @@ package body SDL.Video.Windows is
       C.Strings.Free (C_Title_Str);
    end Create;
 
+   procedure Create (Self : in out Window; Native : in Native_Window) is
+      function SDL_Create_Window_From (Native : Native_Window) return System.Address with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_CreateWindowFrom";
+   begin
+      Self.Internal := SDL_Create_Window_From (Native);
+   end Create;
+
    procedure Finalize (Object : in out Window) is
       procedure SDL_Destroy (W : in System.Address) with
         Import        => True,
@@ -41,12 +53,72 @@ package body SDL.Video.Windows is
       Object.Internal := System.Null_Address;
    end Finalize;
 
-   function Get_Brightness (Self : in Window) return Float is
+   function Get_Brightness (Self : in Window) return Brightness is
       function SDL_Get_Brightness (W : in System.Address) return C.C_float With
         Import        => True,
         Convention    => C,
         External_Name => "SDL_GetWindowBrightness";
    begin
-      return Float (SDL_Get_Brightness (Self.Internal));
+      return Brightness (SDL_Get_Brightness (Self.Internal));
    end Get_Brightness;
+
+   procedure Set_Brightness (Self : in out Window; How_Bright : in Brightness) is
+      function SDL_Set_Brightness (W : in System.Address; B : in C.C_float) return C.int With
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_SetWindowBrightness";
+
+      Result : C.int := SDL_Set_Brightness (Self.Internal, C.C_float (How_Bright));
+   begin
+      if Result /= Success then
+         raise Window_Error with "Unable to set brightness.";
+      end if;
+   end Set_Brightness;
+
+   function To_Data_Access is new Ada.Unchecked_Conversion (Source => System.Address, Target => User_Data_Access);
+   function To_Address is new Ada.Unchecked_Conversion (Source => User_Data_Access, Target => System.Address);
+
+   function Get_Data (Self : in Window; Name : in String) return User_Data_Access is
+      function SDL_Get_Window_Data (W : in System.Address; Name : in C.Strings.chars_ptr) return System.Address with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_GetWindowData";
+
+      C_Name_Str : C.Strings.chars_ptr := C.Strings.New_String (Name);
+      Item       : User_Data_Access    := To_Data_Access (SDL_Get_Window_Data (Self.Internal, C_Name_Str));
+   begin
+      C.Strings.Free (C_Name_Str);
+
+      return Item;
+   end Get_Data;
+
+   function Set_Data (Self : in out Window; Name : in String; Item : in User_Data_Access) return User_Data_Access is
+      function SDL_Set_Window_Data (W : in System.Address; Name : in C.Strings.chars_ptr; User_Data : in System.address)
+                                   return System.Address with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_SetWindowData";
+
+      C_Name_Str    : C.Strings.chars_ptr := C.Strings.New_String (Name);
+      Previous_Data : User_Data_Access    := To_Data_Access (SDL_Set_Window_Data (Self.Internal, C_Name_Str, To_Address (Item)));
+   begin
+      C.Strings.Free (C_Name_Str);
+
+      return Previous_Data;
+   end Set_Data;
+
+   function Display_Index (Self : in Window) return Positive is
+      function SDL_Get_Window_Display_Index (W : in System.Address) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_GetWindowDisplayIndex";
+
+      Total : C.int := SDL_Get_Window_Display_Index (Self.Internal);
+   begin
+      if Total < 0 then
+         raise Window_Error with "Cannot get display index";
+      end if;
+
+      return Positive (Total);
+   end Display_Index;
 end SDL.Video.Windows;
--- src/sdl-video-windows.ads
+++ src/sdl-video-windows.ads
@@ -7,6 +7,8 @@ with Ada.Finalization;
 with System;
 
 package SDL.Video.Windows is
+   Window_Error : exception;
+
    type Window_Flags is mod 2 ** 32 with
      Convention => C;
 
@@ -24,6 +26,16 @@ package SDL.Video.Windows is
    Full_Screen_Desktop : constant Window_Flags := Full_Screen or 16#0000_1000#;
    Foreign             : constant Window_Flags := 16#0000_0800#; --  TODO: Not implemented yet.
 
+   type Native_Window is private;
+
+   --  Allow users to derive new types from this.
+   type User_Data is tagged private;
+
+   type User_Data_Access is access all User_Data'Class;
+
+   --  TODO: Check this type!
+   type Brightness is digits 3 range 0.0 .. 1.0;
+
    --  type Window is tagged limited Private;
    type Window is new Ada.Finalization.Limited_Controlled with private;
 
@@ -36,11 +48,60 @@ package SDL.Video.Windows is
       Height : in Integer;
       Flags  : in Window_Flags := OpenGL);
 
+   --  Create a window from an existing window created in some other way.
+   procedure Create (Self : in out Window; Native : in Native_Window) with
+     Inline => True;
+
    procedure Finalize (Object : in out Window);
 
-   function Get_Brightness (Self : in Window) return Float with
+   function Get_Brightness (Self : in Window) return Brightness with
      Inline => True;
+
+   procedure Set_Brightness (Self : in out Window; How_Bright : in Brightness);
+
+   function Get_Data (Self : in Window; Name : in String) return User_Data_Access;
+   function Set_Data (Self : in out Window; Name : in String; Item : in User_Data_Access) return User_Data_Access ;
+
+   function Display_Index (Self : in Window) return Positive;
+
+   --  SDL_GetWindowDisplayMode
+   --  SDL_GetWindowFlags
+   --  SDL_GetWindowFromID
+   --  SDL_GetWindowGammaRamp
+   --  SDL_GetWindowGrab
+   --  SDL_GetWindowID
+   --  SDL_GetWindowMaximumSize
+   --  SDL_GetWindowMinimumSize
+   --  SDL_GetWindowPixelFormat
+   --  SDL_GetWindowPosition
+   --  SDL_GetWindowSize
+   --  SDL_GetWindowSurface
+   --  SDL_GetWindowTitle
+   --  SDL_GetWindowWMInfo
+   --  SDL_HideWindow
+   --  SDL_MaximizeWindow
+   --  SDL_MinimizeWindow
+   --  SDL_RaiseWindow
+   --  SDL_RestoreWindow
+
+   --  SDL_SetWindowDisplayMode
+   --  SDL_SetWindowFullscreen
+   --  SDL_SetWindowGammaRamp
+   --  SDL_SetWindowGrab
+   --  SDL_SetWindowIcon
+   --  SDL_SetWindowMaximumSize
+   --  SDL_SetWindowMinimumSize
+   --  SDL_SetWindowPosition
+   --  SDL_SetWindowSize
+   --  SDL_SetWindowTitle
+   --  SDL_ShowWindow
+   --  SDL_UpdateWindowSurface
+   --  SDL_UpdateWindowSurfaceRects
 private
+   type Native_Window is new System.Address;
+
+   type User_Data is new Ada.Finalization.Controlled with null record;
+
    type Window is new Ada.Finalization.Limited_Controlled with
       record
          Internal : System.Address := System.Null_Address;
