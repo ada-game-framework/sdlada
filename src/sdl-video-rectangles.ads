c304b8d32130f024656177d58732df71c5fdccd2
Luke A. Guest <laguest@archeia.com>
2013-10-11 16:36:46 +0100

    Initial version of Rectangle support.

---
+++ src/sdl-video-rectangles.ads
@@ -0,0 +1,19 @@
+--                              -*- Mode: Ada -*-
+--  Filename        : sdl-video-rectangles.ads
+--  Description     : Functions relating to rectangles.
+--  Author          : Luke A. Guest
+--  Created On      : Fri Oct 11 11:41:28 2013
+with Interfaces.C;
+
+package SDL.Video.Rectangles is
+   package C renames Interfaces.C;
+
+   type Rectangle is
+      record
+         X      : C.int;
+         Y      : C.int;
+         Width  : C.int;
+         height : C.int;
+      end record with
+        Convention => C;
+end SDL.Video.Rectangles;
