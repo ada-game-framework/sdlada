73705e441d16e09a186de3a76f6e27c6eb29264e
Luke A. Guest <laguest@archeia.com>
2013-10-13 00:36:22 +0100

    Added basic support for renderers.

---
+++ src/sdl-video-renderers.ads
@@ -0,0 +1,44 @@
+--                              -*- Mode: Ada -*-
+--  Filename        : sdl-video-renderers.ads
+--  Description     : Renderer.
+--  Author          : Luke A. Guest
+--  Created On      : Sat Oct 12 21:39:48 2013
+package SDL.Video.Renderers is
+   --  TODO: Finish this.
+
+--  SDL_RendererFlags
+--  SDL_RendererFlip
+--  SDL_RendererInfo
+
+--  SDL_CreateRenderer
+--  SDL_CreateSoftwareRenderer
+--  SDL_CreateWindowAndRenderer
+--  SDL_DestroyRenderer
+--  SDL_DestroyTexture
+--  SDL_GetNumRenderDrivers
+--  SDL_GetRenderDrawBlendMode
+--  SDL_GetRenderDrawColor
+--  SDL_GetRenderDriverInfo
+--  SDL_GetRenderer
+--  SDL_GetRendererInfo
+--  SDL_RenderClear
+--  SDL_RenderCopy
+--  SDL_RenderCopyEx
+--  SDL_RenderDrawLine
+--  SDL_RenderDrawLines
+--  SDL_RenderDrawPoint
+--  SDL_RenderDrawPoints
+--  SDL_RenderDrawRect
+--  SDL_RenderDrawRects
+--  SDL_RenderFillRect
+--  SDL_RenderFillRects
+--  SDL_RenderGetClipRect
+--  SDL_RenderGetViewport
+--  SDL_RenderPresent
+--  SDL_RenderReadPixels
+--  SDL_RenderSetClipRect
+--  SDL_RenderSetViewport
+--  SDL_SetRenderDrawBlendMode
+--  SDL_SetRenderDrawColor
+--  SDL_SetRenderTarget
+end SDL.Video.Renderers;
