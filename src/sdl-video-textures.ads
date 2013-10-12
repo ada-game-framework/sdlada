1508dfc3d0d7a4c7b16814fbfad2a8ac0b3cbe1c
Luke A. Guest <laguest@archeia.com>
2013-10-13 00:36:39 +0100

    Added basic support for textures.

---
+++ src/sdl-video-textures.ads
@@ -0,0 +1,24 @@
+--                              -*- Mode: Ada -*-
+--  Filename        : sdl-video-textures.ads
+--  Description     : Texture abstraction.
+--  Author          : Luke A. Guest
+--  Created On      : Sat Oct 12 21:34:12 2013
+package SDL.Video.Textures is
+   --  TODO: Finish this.
+--  SDL_TextureAccess
+--  SDL_TextureModulate
+
+--  SDL_CreateTexture
+--  SDL_CreateTextureFromSurface
+--  SDL_DestroyTexture
+--  SDL_GetTextureAlphaMod
+--  SDL_GetTextureBlendMode
+--  SDL_GetTextureColorMod
+--  SDL_LockTexture
+--  SDL_QueryTexture
+--  SDL_SetTextureAlphaMod
+--  SDL_SetTextureBlendMode
+--  SDL_SetTextureColorMod
+--  SDL_UnlockTexture
+--  SDL_UpdateTexture
+end SDL.Video.Textures;
