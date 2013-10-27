fc124fb7e90af3ae629ad3f68e77c6ab641fa499
Luke A. Guest <laguest@archeia.com>
2013-10-27 18:11:38 +0000

    Initial version of the texture API.

---
+++ src/sdl-video-textures.adb
@@ -0,0 +1,169 @@
+with Interfaces.C;
+with SDL.Error;
+
+package body SDL.Video.Textures is
+   package C renames Interfaces.C;
+
+   use type C.int;
+   use type System.Address;
+
+   --  We need to get the actual adresses of these objects in this package.
+   function Get_Address (Self : in SDL.Video.Renderers.Renderer) return System.Address with
+     Import     => True,
+     Convention => Ada;
+
+   function Get_Address (Self : in SDL.Video.Surfaces.Surface) return System.Address with
+     Import     => True,
+     Convention => Ada;
+
+   procedure Create
+     (Self     : in out Texture;
+      Renderer : in SDL.Video.Renderers.Renderer;
+      Format   : in SDL.Video.Pixel_Formats.Pixel_Format;
+      Kind     : in Kinds;
+      Size     : in SDL.Video.Windows.Sizes) is
+
+      function SDL_Create_Texture
+        (R      : in System.Address;
+         Format : in SDL.Video.Pixel_Formats.Pixel_Format;
+         Kind   : in Kinds;
+         W, H   : in C.int) return System.Address with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_CreateTexture";
+   begin
+      Self.Internal := SDL_Create_Texture (Get_Address (Renderer), Format, Kind, C.int (Size.Width), C.int (Size.Height));
+
+      if Self.Internal = System.Null_Address then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+   end Create;
+
+   procedure Create
+     (Self     : in out Texture;
+      Renderer : in SDL.Video.Renderers.Renderer;
+      Surface  : in SDL.Video.Surfaces.Surface) is
+
+      function SDL_Create_Texture_Form_Surface (R, S : in System.Address) return System.Address with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_CreateTextureFromSurface";
+   begin
+      Self.Internal := SDL_Create_Texture_Form_Surface (Get_Address (Renderer), Get_Address (Surface));
+
+      if Self.Internal = System.Null_Address then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+   end Create;
+
+   procedure Destroy (Self : in out Texture) is
+      procedure SDL_Destroy_Texture (T : in System.Address) with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_DestroyTexture";
+   begin
+      SDL_Destroy_Texture (Self.Internal);
+
+      Self.Internal := System.Null_Address;
+   end Destroy;
+
+   function Get_Alpha (Self : in Texture) return SDL.Video.Palettes.Colour_Component is
+      function SDL_Get_Texture_Alpha_Mod (T     : in System.Address;
+                                          Alpha : out SDL.Video.Palettes.Colour_Component) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_GetTextureAlphaMod";
+
+      Data   : SDL.Video.Palettes.Colour_Component;
+      Result : C.int := SDL_Get_Texture_Alpha_Mod (Self.Internal, Data);
+   begin
+      if Result /= Success then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+
+      return Data;
+   end Get_Alpha;
+
+   procedure Set_Alpha (Self : in out Texture; Alpha : in SDL.Video.Palettes.Colour_Component) is
+      function SDL_Set_Texture_Alpha_Mod (T     : in System.Address;
+                                          Alpha : in SDL.Video.Palettes.Colour_Component) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_SetTextureAlphaMod";
+
+      Result : C.int := SDL_Set_Texture_Alpha_Mod (Self.Internal, Alpha);
+   begin
+      if Result /= Success then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+   end Set_Alpha;
+
+   function Get_Blend_Mode (Self : in Texture) return Blend_Modes is
+      function SDL_Get_Texture_Blend_Mode (T     : in System.Address;
+                                           Blend : out Blend_Modes) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_GetTextureBlendMode";
+
+      Data   : Blend_Modes;
+      Result : C.int := SDL_Get_Texture_Blend_Mode (Self.Internal, Data);
+   begin
+      if Result /= Success then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+
+      return Data;
+   end Get_Blend_Mode;
+
+   procedure Set_Blend_Mode (Self : in out Texture; Mode : in Blend_Modes) is
+      function SDL_Set_Texture_Blend_Mode (T    : in System.Address;
+                                           Mode : in Blend_Modes) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_SetTextureBlendMode";
+
+      Result : C.int := SDL_Set_Texture_Blend_Mode (Self.Internal, Mode);
+   begin
+      if Result /= Success then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+   end Set_Blend_Mode;
+
+   function Get_Colour (Self : in Texture) return SDL.Video.Palettes.RGB_Colour is
+      function SDL_Get_Texture_Color_Mod (T       : in System.Address;
+                                          R, G, B : out SDL.Video.Palettes.Colour_Component) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_GetTextureColorMod";
+
+      Data   : SDL.Video.Palettes.RGB_Colour;
+      Result : C.int := SDL_Get_Texture_Color_Mod (Self.Internal, Data.Red, Data.Green, Data.Blue);
+   begin
+      if Result /= Success then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+
+      return Data;
+   end Get_Colour;
+
+   procedure Set_Colour (Self : in out Texture; Colour : in SDL.Video.Palettes.RGB_Colour) is
+      function SDL_Set_Texture_Color_Mod (T       : in System.Address;
+                                          R, G, B : in SDL.Video.Palettes.Colour_Component) return C.int with
+        Import        => True,
+        Convention    => C,
+        External_Name => "SDL_SetTextureColorMod";
+
+      Result : C.int := SDL_Set_Texture_Color_Mod (Self.Internal, Colour.Red, Colour.Green, Colour.Blue);
+   begin
+      if Result /= Success then
+         raise Texture_Error with SDL.Error.Get;
+      end if;
+   end Set_Colour;
+
+   procedure Finalize (Self : in out Texture) is
+   begin
+      if Self.Internal /= System.Null_Address then
+         Destroy (Self);
+      end if;
+   end Finalize;
+end SDL.Video.Textures;
--- src/sdl-video-textures.ads
+++ src/sdl-video-textures.ads
@@ -3,22 +3,80 @@
 --  Description     : Texture abstraction.
 --  Author          : Luke A. Guest
 --  Created On      : Sat Oct 12 21:34:12 2013
-package SDL.Video.Textures is
-   --  TODO: Finish this.
---  SDL_TextureAccess
---  SDL_TextureModulate
+with Ada.Finalization;
+with System;
+with SDL.Video.Palettes;
+with SDL.Video.Pixel_Formats;
+with SDL.Video.Rectangles;
+with SDL.Video.Renderers;
+with SDL.Video.Surfaces;
+with SDL.Video.Windows;
 
---  SDL_CreateTexture
---  SDL_CreateTextureFromSurface
---  SDL_DestroyTexture
---  SDL_GetTextureAlphaMod
---  SDL_GetTextureBlendMode
---  SDL_GetTextureColorMod
+package SDL.Video.Textures is
+   Texture_Error : exception;
+
+   --  Was SDL_TextureAccess.
+   type Kinds is (Static, Streaming, Target) with
+     Convention => C;
+
+   type Blend_Modes is range 0 .. 4 with
+     Convention => C;
+
+   Blend_None     : constant Blend_Modes := 16#0000_0000#;
+   Blend_Alpha    : constant Blend_Modes := 16#0000_0001#;
+   Blend_Additive : constant Blend_Modes := 16#0000_0002#;
+   Blend_Modulate : constant Blend_Modes := 16#0000_0004#;
+
+   --  Was SDL_TextureModulate
+
+   type Texture is new Ada.Finalization.Limited_Controlled with private;
+
+   Null_Texture : constant Texture;
+
+   procedure Create
+     (Self     : in out Texture;
+      Renderer : in SDL.Video.Renderers.Renderer;
+      Format   : in SDL.Video.Pixel_Formats.Pixel_Format;
+      Kind     : in Kinds;
+      Size     : in SDL.Video.Windows.Sizes);
+
+   procedure Create
+     (Self     : in out Texture;
+      Renderer : in SDL.Video.Renderers.Renderer;
+      Surface  : in SDL.Video.Surfaces.Surface);
+
+   procedure Destroy (Self : in out Texture);
+
+   --  Get the alpha value to be multiplied (modulated) into render copy operations.
+   function Get_Alpha (Self : in Texture) return SDL.Video.Palettes.Colour_Component;
+   procedure Set_Alpha (Self : in out Texture; Alpha : in SDL.Video.Palettes.Colour_Component);
+
+   function Get_Blend_Mode (Self : in Texture) return Blend_Modes;
+   procedure Set_Blend_Mode (Self : in out Texture; Mode : in Blend_Modes);
+
+   function Get_Colour (Self : in Texture) return SDL.Video.Palettes.RGB_Colour;
+   procedure Set_Colour (Self : in out Texture; Colour : in SDL.Video.Palettes.RGB_Colour);
+
+   --  Lock returns access to pixel data as write-only.
+   --  function Lock (Self : in out Texture; Pixel_Data : out SDL.Video.Pixels.Pixel) return Boolean with
+   --  function Lock (Self : in out Texture; Area : in SDL.Video.Rectangles.Rectangle; Pixel_Data : out SDL.Video.Pixels.Pixel) return Boolean with
+   --    Pre  => Self.Locked = False,
+   --    Post => Result = True and then Self.Locked = True;
 --  SDL_LockTexture
---  SDL_QueryTexture
---  SDL_SetTextureAlphaMod
---  SDL_SetTextureBlendMode
---  SDL_SetTextureColorMod
 --  SDL_UnlockTexture
+--  SDL_QueryTexture
 --  SDL_UpdateTexture
+private
+   type Texture is new Ada.Finalization.Limited_Controlled with
+      record
+         Internal : System.Address := System.Null_Address;
+         Locked   : Boolean        := False;
+      end record;
+
+   overriding
+   procedure Finalize (Self : in out Texture);
+
+   Null_Texture : constant Texture := (Ada.Finalization.Limited_Controlled with
+                                         Internal => System.Null_Address,
+                                         Locked   => False);
 end SDL.Video.Textures;
