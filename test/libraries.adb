634ab4b544d6b51794154fe1b6d499b09c202bd0
Luke A. Guest <laguest@archeia.com>
2013-10-30 13:54:39 +0000

    Added the test body which calls two functions, one written in C, the other in Ada, but both from a shared lib.

--- test/libraries.adb
+++ test/libraries.adb
@@ -1,14 +1,39 @@
 with SDL;
 with SDL.Error;
+with SDL.Libraries;
 with SDL.Log;
 with System;
 
 use type System.Bit_Order;
 
+--  Run with: LD_LIBRARY_PATH=./build_test:$LD_LIBRARY_PATH ./build_test/libraries
+
 procedure Libraries is
+   Lib : SDL.Libraries.Handles;
 begin
+   SDL.Libraries.Load (Lib, "libtestmaths.so");
+
    SDL.Log.Set (Category => SDL.Log.Application, Priority => SDL.Log.Debug);
 
+   declare
+      type Access_To_Add is access function (A, B : in Integer) return Integer;
+      type Access_To_Sub is access function (A, B : in Integer) return Integer with
+        Convention => C;
+
+      function Load is new SDL.Libraries.Load_Sub_Program
+        (Access_To_Sub_Program => Access_To_Add,
+         Name                  => "Add");
+
+      function Load is new SDL.Libraries.Load_Sub_Program
+        (Access_To_Sub_Program => Access_To_Sub,
+         Name                  => "sub");
+
+      Add : Access_To_Add := Load (From_Library => Lib);
+      Sub : Access_To_Sub := Load (From_Library => Lib);
+   begin
+      SDL.Log.Put_Debug ("1 + 2 = " & Integer'Image (Add (1, 2)));
+      SDL.Log.Put_Debug ("5 - 1 = " & Integer'Image (Sub (5, 1)));
+   end;
 
    SDL.Finalise;
 end Libraries;
