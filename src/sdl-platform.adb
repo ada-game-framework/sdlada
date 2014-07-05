5abcd5d1af858b8c2b3a14ada8995668ea6dbd14
Luke A. Guest <laguest@archeia.com>
2014-07-05 18:06:45 +0100

    Added an explanation comment to the Get function.

--- src/sdl-platform.adb
+++ src/sdl-platform.adb
@@ -21,5 +21,7 @@
 --     distribution.
 --------------------------------------------------------------------------------------------------------------------
 package body SDL.Platform is
+   --  Bring in the platform specific version for each build of the library. It's also defined as inline so
+   --  that the compiler can eliminate redundant code in the static expressions that use this function.
    function Get return Platforms is separate;
 end SDL.Platform;
