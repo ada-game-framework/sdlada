ca6f1d483ffb63d1089cc1a84bb9806ab81965ba
Luke A. Guest <laguest@archeia.com>
2015-03-09 17:55:47 +0000

    A new test case to make sure colours are represented properly.

---
+++ unit_tests/colour_test_cases.adb
@@ -0,0 +1,46 @@
+--------------------------------------------------------------------------------------------------------------------
+--  Copyright (c) 2014-2015 Luke A. Guest
+--
+--  This software is provided 'as-is', without any express or implied
+--  warranty. In no event will the authors be held liable for any damages
+--  arising from the use of this software.
+--
+--  Permission is granted to anyone to use this software for any purpose,
+--  including commercial applications, and to alter it and redistribute it
+--  freely, subject to the following restrictions:
+--
+--     1. The origin of this software must not be misrepresented; you must not
+--     claim that you wrote the original software. If you use this software
+--     in a product, an acknowledgment in the product documentation would be
+--     appreciated but is not required.
+--
+--     2. Altered source versions must be plainly marked as such, and must not be
+--     misrepresented as being the original software.
+--
+--     3. This notice may not be removed or altered from any source
+--     distribution.
+--------------------------------------------------------------------------------------------------------------------
+--  Colour_Test_Cases
+--------------------------------------------------------------------------------------------------------------------
+with AUnit.Assertions; use AUnit.Assertions;
+with SDL.Video.Palettes;
+
+package body Colour_Test_Cases is
+   overriding
+   function Name (Test : Colour_Test_Case) return Message_String is
+   begin
+      return Format ("Colour test");
+   end Name;
+
+   overriding
+   procedure Run_Test (Test : in out Colour_Test_Case) is
+      use type SDL.Video.Palettes.Colour_Component;
+
+      Colour : SDL.Video.Palettes.Colour := (Red => 16#FF#, Green => 16#DD#, Blue => 16#AA#, Alpha => 16#88#);
+   begin
+      Assert (Colour.Red = C_Test.Red, "Red values do not match");
+      Assert (Colour.Green = C_Test.Green, "Green values do not match");
+      Assert (Colour.Blue = C_Test.Blue, "Blue values do not match");
+      Assert (Colour.Alpha = C_Test.Alpha, "Alpha values do not match");
+   end Run_Test;
+end Colour_Test_Cases;
---
+++ unit_tests/colour_test_cases.ads
@@ -0,0 +1,44 @@
+--------------------------------------------------------------------------------------------------------------------
+--  Copyright (c) 2014-2015 Luke A. Guest
+--
+--  This software is provided 'as-is', without any express or implied
+--  warranty. In no event will the authors be held liable for any damages
+--  arising from the use of this software.
+--
+--  Permission is granted to anyone to use this software for any purpose,
+--  including commercial applications, and to alter it and redistribute it
+--  freely, subject to the following restrictions:
+--
+--     1. The origin of this software must not be misrepresented; you must not
+--     claim that you wrote the original software. If you use this software
+--     in a product, an acknowledgment in the product documentation would be
+--     appreciated but is not required.
+--
+--     2. Altered source versions must be plainly marked as such, and must not be
+--     misrepresented as being the original software.
+--
+--     3. This notice may not be removed or altered from any source
+--     distribution.
+--------------------------------------------------------------------------------------------------------------------
+--  Colour_Test_Cases
+--
+--  Tests to check whether the memory layout of the pixel formats is correct.
+--------------------------------------------------------------------------------------------------------------------
+with AUnit; use AUnit;
+with AUnit.Simple_Test_Cases;
+with SDL.Video.Palettes;
+
+package Colour_Test_Cases is
+   type Colour_Test_Case is new AUnit.Simple_Test_Cases.Test_Case with null record;
+
+   overriding
+   function Name (Test : Colour_Test_Case) return Message_String;
+
+   overriding
+   procedure Run_Test (Test : in out Colour_Test_Case);
+private
+   C_Test : constant SDL.Video.Palettes.Colour with
+     Import        => True,
+     Convention    => C,
+     External_Name => "test_colour";
+end Colour_Test_Cases;
