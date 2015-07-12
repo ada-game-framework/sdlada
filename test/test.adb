40ca62fbf967f96f1f4eda0cb65f140ee6a0690c
Luke A. Guest <laguest@archeia.com>
2015-07-12 15:17:21 +0100

    Add machine word size. Reformatting of output.

--- test/test.adb
+++ test/test.adb
@@ -17,37 +17,38 @@ begin
 
    SDL.Versions.Linked_With (Info => Linked_Version);
 
-   SDL.Log.Put_Debug ("Revision       : " & SDL.Versions.Revision);
-   SDL.Log.Put_Debug ("Linked with    : " & SDL.Versions.Version_Level'Image (Linked_Version.Major) &
+   SDL.Log.Put_Debug ("System.Word_Size: " & Integer'Image (System.Word_Size));
+   SDL.Log.Put_Debug ("Revision        : " & SDL.Versions.Revision);
+   SDL.Log.Put_Debug ("Linked with     : " & SDL.Versions.Version_Level'Image (Linked_Version.Major) &
                         "." & SDL.Versions.Version_Level'Image (Linked_Version.Minor) &
                         "." & SDL.Versions.Version_Level'Image (Linked_Version.Patch));
-   SDL.Log.Put_Debug ("Compiled with  : " & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Major) &
+   SDL.Log.Put_Debug ("Compiled with   : " & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Major) &
                         "." & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Minor) &
                         "." & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Patch));
-   SDL.Log.Put_Debug ("Bit Order      : " & System.Bit_Order'Image (SDL.Video.Windows.Window'Bit_Order));
-   SDL.Log.Put_Debug ("Total drivers  : " & Positive'Image (Total_Drivers));
+   SDL.Log.Put_Debug ("Bit Order       : " & System.Bit_Order'Image (SDL.Video.Windows.Window'Bit_Order));
+   SDL.Log.Put_Debug ("Total drivers   : " & Positive'Image (Total_Drivers));
 
    for Index in Positive'First .. Total_Drivers loop
-      SDL.Log.Put_Debug ("Driver (" & Positive'Image (Index) & ")    : " & SDL.Video.Driver_Name (Natural (Index)));
+      SDL.Log.Put_Debug ("Driver (" & Positive'Image (Index) & ")     : " & SDL.Video.Driver_Name (Natural (Index)));
    end loop;
 
    if SDL.Initialise = True then
-      SDL.Log.Put_Debug ("Current driver : " & SDL.Video.Current_Driver_Name);
-      SDL.Log.Put_Debug ("Total displays : " & Positive'Image (SDL.Video.Total_Displays));
+      SDL.Log.Put_Debug ("Current driver  : " & SDL.Video.Current_Driver_Name);
+      SDL.Log.Put_Debug ("Total displays  : " & Positive'Image (SDL.Video.Total_Displays));
 
       SDL.Error.Clear;
 
-      SDL.Log.Put_Debug ("Error          : " & SDL.Error.Get);
+      SDL.Log.Put_Debug ("Error           : " & SDL.Error.Get);
 
       W.Create (Title => "Test SDLAda 2.0 - हिन्दी समाचार", X => 100, Y => 100, Width => 800, Height => 640);
 
-      SDL.Log.Put_Debug ("Window Grabbed : " & Boolean'Image (W.Is_Grabbed));
+      SDL.Log.Put_Debug ("Window Grabbed  : " & Boolean'Image (W.Is_Grabbed));
 
       --  W.Set_Grabbed;
 
-      SDL.Log.Put_Debug ("Window Grabbed : " & Boolean'Image (W.Is_Grabbed));
-      SDL.Log.Put_Debug ("Window ID      : " & SDL.Video.Windows.ID'Image (W.Get_ID));
-      SDL.Log.Put_Debug ("Window Title   : " & W.Get_Title);
+      SDL.Log.Put_Debug ("Window Grabbed  : " & Boolean'Image (W.Is_Grabbed));
+      SDL.Log.Put_Debug ("Window ID       : " & SDL.Video.Windows.ID'Image (W.Get_ID));
+      SDL.Log.Put_Debug ("Window Title    : " & W.Get_Title);
 
       --  W.Set_Mode (SDL.Video.Windows.Full_Screen);
 
@@ -65,7 +66,7 @@ begin
                      Finished := True;
 
                   when SDL.Events.Keyboards.Key_Up =>
-                     SDL.Log.Put_Debug ("Key up event: " &
+                     SDL.Log.Put_Debug ("Key up event    : " &
                                           SDL.Events.Keyboards.Key_Codes'Image (Event.Keyboard.Key_Sym.Key_Code) &
                                           "    Scan code: " &
                                           SDL.Events.Keyboards.Scan_Codes'Image (Event.Keyboard.Key_Sym.Scan_Code));
