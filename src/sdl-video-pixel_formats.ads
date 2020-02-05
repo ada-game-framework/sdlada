9c0726411af26f048ced757cec4bb720a5e6bb52
Jesper Quorning <jesper.quorning@gmail.com>
2020-02-05 16:04:05 +0100

    Avoid user Interfaces with.

--- src/sdl-video-pixel_formats.ads
+++ src/sdl-video-pixel_formats.ads
@@ -33,6 +33,10 @@ with SDL.Video.Palettes;
 package SDL.Video.Pixel_Formats is
    package C renames Interfaces.C;
 
+   subtype Unsigned_8  is Interfaces.Unsigned_8;
+   subtype Unsigned_16 is Interfaces.Unsigned_16;
+   subtype Unsigned_32 is Interfaces.Unsigned_32;
+
    type Pixel_Types is
      (Unknown,
       Index_1,
