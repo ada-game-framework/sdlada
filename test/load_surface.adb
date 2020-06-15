af50767d4f73b624d502f6f0c6dde2df7e931880
Luke A. Guest <laguest@archeia.com>
2020-06-15 21:55:43 +0100

    Fix style warnings:   Line too long.   Space missing.

--- test/load_surface.adb
+++ test/load_surface.adb
@@ -29,19 +29,23 @@ begin
          Event              : SDL.Events.Events.Events;
          Window_Surface     : SDL.Video.Surfaces.Surface;
          Image_Surface      : SDL.Video.Surfaces.Surface;
-         Image_Area         : SDL.Video.Rectangles.Rectangle := (X => 0, Y => 0, Width => 400, Height => 300);
-         Image_Dest_Area    : SDL.Video.Rectangles.Rectangle := (X      => Window_Size.Width / 2 - Image_Area.Width / 2,
-                                                                 Y      => Window_Size.Height / 2 - Image_Area.Height / 2,
-                                                                 Width  => 400,
-                                                                 Height => 300);
-         Scaled_Dest_Area   : SDL.Video.Rectangles.Rectangle := (X      => 10,
-                                                                 Y      => 10,
-                                                                 Width  => Image_Area.Width / 4,
-                                                                 Height => Image_Area.Height / 4);
-         Scaled_Dest_Area_2 : SDL.Video.Rectangles.Rectangle := (X      => 10,
-                                                                 Y      => 100,
-                                                                 Width  => Image_Area.Width / 4,
-                                                                 Height => Image_Area.Height / 4);
+         Image_Area         : SDL.Video.Rectangles.Rectangle :=
+           (X => 0, Y => 0, Width => 400, Height => 300);
+         Image_Dest_Area    : SDL.Video.Rectangles.Rectangle :=
+           (X      => Window_Size.Width / 2 - Image_Area.Width / 2,
+            Y      => Window_Size.Height / 2 - Image_Area.Height / 2,
+            Width  => 400,
+            Height => 300);
+         Scaled_Dest_Area   : SDL.Video.Rectangles.Rectangle :=
+           (X      => 10,
+            Y      => 10,
+            Width  => Image_Area.Width / 4,
+            Height => Image_Area.Height / 4);
+         Scaled_Dest_Area_2 : SDL.Video.Rectangles.Rectangle :=
+           (X      => 10,
+            Y      => 100,
+            Width  => Image_Area.Width / 4,
+            Height => Image_Area.Height / 4);
          Finished           : Boolean := False;
 
          use type SDL.Events.Keyboards.Key_Codes;
@@ -63,7 +67,7 @@ begin
                                      Source_Area => SDL.Video.Rectangles.Rectangle'(X      => 0,
                                                                                     Y      => 0,
                                                                                     Width  => Image_Area.Width / 2,
-                                                                                    Height => Image_Area.Height /2));
+                                                                                    Height => Image_Area.Height / 2));
 
          W.Update_Surface;
 
