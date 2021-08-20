e4f694561e04e6be7324ce02a30a1e9e0e54c467
Dmitriy Anisimkov <anisimko@adacore.com>
2021-08-20 06:35:16 +0600

    Fix to build on GNAT Community 2021

--- src/sdl-video-palettes.ads
+++ src/sdl-video-palettes.ads
@@ -121,7 +121,7 @@ private
          Data : Internal_Palette_Access;
       end record;
 
-   type Palette_Constant_Access is access constant Palette;
+   type Palette_Constant_Access is access constant Palette'Class;
 
    type Cursor is
       record
