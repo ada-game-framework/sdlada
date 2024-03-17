d1ad6a19e87eeccb44f53a240b7c31d74598a631
Luke A. Guest <laguest@archeia.com>
2024-03-17 13:13:12 +0000

    Rename Enable_Screen -> Enable_Video. Add renamed constant

--- src/sdl.ads
+++ src/sdl.ads
@@ -20,15 +20,17 @@ package SDL is
    Null_Init_Flags        : constant Init_Flags := 16#0000_0000#;
    Enable_Timer           : constant Init_Flags := 16#0000_0001#;
    Enable_Audio           : constant Init_Flags := 16#0000_0010#;
-   Enable_Screen          : constant Init_Flags := 16#0000_0020#;
+   Enable_Video           : constant Init_Flags := 16#0000_0020#;
    Enable_Joystick        : constant Init_Flags := 16#0000_0200#;
    Enable_Haptic          : constant Init_Flags := 16#0000_1000#;
    Enable_Game_Controller : constant Init_Flags := 16#0000_2000#;
    Enable_Events          : constant Init_Flags := 16#0000_4000#;
    Enable_No_Parachute    : constant Init_Flags := 16#0010_0000#;
    Enable_Everything      : constant Init_Flags :=
-     Enable_Timer or Enable_Audio or Enable_Screen or Enable_Joystick or Enable_Haptic or
+     Enable_Timer or Enable_Audio or Enable_Video or Enable_Joystick or Enable_Haptic or
      Enable_Game_Controller or Enable_Events or Enable_No_Parachute;
+
+   Enable_Screen renames Enable_Video;  --  Deprecated.
 
    --  Coordinates are for positioning things.
    subtype Coordinate is C.int;
