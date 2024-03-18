0ce85f6b0de53846ea95342d95e0287a081fdf72
Luke A. Guest <laguest@archeia.com>
2024-03-18 11:41:26 +0000

    Use new object 'Image attributes. Add Trim to remove sign bit space.

--- test/version.adb
+++ test/version.adb
@@ -1,11 +1,14 @@
 --------------------------------------------------------------------------------------------------------------------
 --  This source code is subject to the Zlib license, see the LICENCE file in the root of this directory.
 --------------------------------------------------------------------------------------------------------------------
+with Ada.Strings.Fixed;
 with SDL;
 with SDL.Log;
 with SDL.Versions;
 
 procedure Version is
+   package ASF renames Ada.Strings.Fixed;
+
    Linked_Version : SDL.Versions.Version;
 begin
    SDL.Log.Set (Category => SDL.Log.Application, Priority => SDL.Log.Debug);
@@ -13,11 +16,11 @@ begin
    SDL.Versions.Linked_With (Info => Linked_Version);
 
    SDL.Log.Put_Debug ("Revision       : " & SDL.Versions.Revision);
-   SDL.Log.Put_Debug ("Linked with    : " & SDL.Versions.Version_Level'Image (Linked_Version.Major) &
-                        "." & SDL.Versions.Version_Level'Image (Linked_Version.Minor) &
-                        "." & SDL.Versions.Version_Level'Image (Linked_Version.Patch));
-   SDL.Log.Put_Debug ("Compiled with  : " & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Major) &
-                        "." & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Minor) &
-                        "." & SDL.Versions.Version_Level'Image (SDL.Versions.Compiled_Patch));
+   SDL.Log.Put_Debug ("Linked with    : " & ASF.Trim (Linked_Version.Major'Image, Ada.Strings.Left) &
+                        "." & ASF.Trim (Linked_Version.Minor'Image, Ada.Strings.Left) &
+                        "." & ASF.Trim (Linked_Version.Patch'Image, Ada.Strings.Left));
+   SDL.Log.Put_Debug ("Compiled with  : " & ASF.Trim (SDL.Versions.Compiled_Major'Image, Ada.Strings.Left) &
+                        "." & ASF.Trim (SDL.Versions.Compiled_Minor'Image, Ada.Strings.Left) &
+                        "." & ASF.Trim (SDL.Versions.Compiled_Patch'Image, Ada.Strings.Left));
    SDL.Finalise;
 end Version;
