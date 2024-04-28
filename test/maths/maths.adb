618d2e7e918f22e5111720445ae3743710a3e700
Luke A. Guest <laguest@archeia.com>
2024-04-28 15:46:10 +0100

    Remove all old test programs now there in their own repo. Remove outdated gpr files also.

--- test/maths/maths.adb
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------------------------------------------------------------------
---  This source code is subject to the Zlib license, see the LICENCE file in the root of this directory.
---------------------------------------------------------------------------------------------------------------------
-package body Maths is
-   function Add (A, B : in Integer) return Integer is
-   begin
-      return A + B;
-   end Add;
-end Maths;
