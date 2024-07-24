8ec894ecc5539e67f15b0370fb1b3383d10f47f7
Luke A. Guest <laguest@archeia.com>
2024-07-24 14:18:13 +0100

    Add SDL_GetTicks64. Obsolete the old function.

--- src/sdl-timers.ads
+++ src/sdl-timers.ads
@@ -10,18 +10,24 @@ with Interfaces;
 package SDL.Timers is
    pragma Pure;
 
+   type Milliseconds_Long is new Interfaces.Unsigned_64;
    type Milliseconds is new Interfaces.Unsigned_32;
 
    --  Return the number of milliseconds since the SDL library initialization.
    function Ticks return Milliseconds with
      Import        => True,
      Convention    => C,
-     External_Name => "SDL_GetTicks";
+     External_Name => "SDL_GetTicks",
+     Obsolescent;
+
+   function Ticks return Milliseconds_Long with
+     Import        => True,
+     Convention    => C,
+     External_Name => "SDL_GetTicks64";
 
    --  Wait a specified number of milliseconds before returning.
    procedure Wait_Delay (MS : Milliseconds) with
      Import        => True,
      Convention    => C,
      External_Name => "SDL_Delay";
-
 end SDL.Timers;
