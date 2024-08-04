7a11de10e347994e5af8b2cfc07d433165b3c81f
Luke A. Guest <laguest@archeia.com>
2024-08-04 13:48:23 +0100

    Add the SDL_BLENDMODE_MUL blending mode.

--- src/video/sdl-video.ads
+++ src/video/sdl-video.ads
@@ -16,14 +16,15 @@ package SDL.Video is
 
    --  subtype Coordinate is C.int;
 
-   type Blend_Modes is (None, Alpha_Blend, Additive, Colour_Modulate) with
+   type Blend_Modes is (None, Alpha_Blend, Additive, Colour_Modulate, Multiply) with
      Convention => C;
 
    for Blend_Modes use
      (None            => 16#0000_0000#,
       Alpha_Blend     => 16#0000_0001#,
       Additive        => 16#0000_0002#,
-      Colour_Modulate => 16#0000_0004#);
+      Colour_Modulate => 16#0000_0004#,
+      Multiply        => 16#0000_0008#);
 
    --  Screen saver information.
    procedure Enable_Screen_Saver with
